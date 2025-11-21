fe2248bfcfa0a1eb5a20334571e30184d673a542
Cutecookies <cat.O1204@yandex.ru>
2025-11-21 11:49:21 +0300

    Add basic main page with gym statistics

--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Client, Trainer, WorkoutClass
 
-# Create your views here.
+# Create views
+def home(request):
+    # Amount entries
+    context = {
+        'total_clients': Client.objects.count(),       # amount clients
+        'total_trainers': Trainer.objects.count(),     # amount trainers
+        'total_classes': WorkoutClass.objects.count(), # amount classes
+    }
+    return render(request, 'gym/home.html', context)
